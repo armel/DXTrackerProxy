d7eee6db2ee1b002162307c067bf15050eeaf364
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-02-01 23:48:09 +0100

    Add Cluster to improve stability

--- DXCluster.py
+++ DXCluster.py
@@ -19,7 +19,7 @@ version = '0.0.1'   # Version
 # Settings
 url = 'https://www.hamqth.com/dxc_csv.php?limit=50'
 
-print('Content-Type: text/html\n\n')
+print('Content-Type: text/html\n')
 
 urllib3.disable_warnings()
 http = urllib3.PoolManager(timeout=1.0)
@@ -36,7 +36,7 @@ try:
 except:
     exit()
 
-print(page)
+print(page, end='')
 
 # End properly
 
