45c073362080289dbb07bd61f4fab15c8d4fb441
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-02-02 02:15:11 +0100

    Try to optimize

--- DXSunmap.py
+++ DXSunmap.py
@@ -41,7 +41,7 @@ fd_img = open(img_input, 'rb')
 
 img = Image.open(fd_img)
 img = resizeimage.resize_cover(img, [320, 160])
-img.save(img_output, format = img.format, quality = 75, optimize = True)
+img.save(img_output, format = img.format, quality = 70, optimize = True)
 
 fd_img.close()
 
