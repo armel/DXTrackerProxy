ac5db0876c1dca4657f2b320544c5fa7b367ee98
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-06-22 17:51:56 +0200

    Add nightmap

--- DXSunmap.py
+++ DXSunmap.py
@@ -19,9 +19,9 @@ version = '0.0.2'   # Version
 
 # Settings
 img_url = 'https://www.timeanddate.com/scripts/sunmap.php'
-img_input = '/tmp/sunmap_original.jpg'
-img_output = '/var/www/html/sunmap.jpg'
-img_output_big = '/var/www/html/sunmapbig.jpg'
+img_input = ['/tmp/sunmap_original.jpg', '/tmp/nightmap_original.jpg']
+img_output = ['/var/www/html/sunmap.jpg', '/var/www/html/nightmap.jpg'] 
+img_output_big = ['/var/www/html/sunmapbig.jpg', '/var/www/html/nightmapbig.jpg']
 
 urllib3.disable_warnings()
 http = urllib3.PoolManager(timeout=1.0)
@@ -30,29 +30,30 @@ http = urllib3.PoolManager(
 )
 
 # Requete HTTP vers le flux heavens-above
-with open(img_input, 'wb') as f:
-    try:
-        r = http.request('GET', img_url, timeout=2)
-        f.write(r.data)
-    except:
-        f.close()
-        exit()
+for earth in range(2):
+    with open(img_input[earth], 'wb') as f:
+        try:
+            r = http.request('GET', img_url + '?earth=' + str(earth), timeout=2)
+            f.write(r.data)
+        except:
+            f.close()
+            exit()
 
-fd_img = open(img_input, 'rb')
+    fd_img = open(img_input[earth], 'rb')
 
-img = Image.open(fd_img)
-img = resizeimage.resize_cover(img, [320, 160])
-img.save(img_output, format = img.format, quality = 70, optimize = True)
+    img = Image.open(fd_img)
+    img = resizeimage.resize_cover(img, [320, 160])
+    img.save(img_output[earth], format = img.format, quality = 70, optimize = True)
 
-fd_img.close()
+    fd_img.close()
 
-fd_img = open(img_input, 'rb')
+    fd_img = open(img_input[earth], 'rb')
 
-img = Image.open(fd_img)
-img = resizeimage.resize_cover(img, [1024, 512])
-img.save(img_output_big, format = img.format, quality = 40, optimize = True)
+    img = Image.open(fd_img)
+    img = resizeimage.resize_cover(img, [1024, 512])
+    img.save(img_output_big[earth], format = img.format, quality = 40, optimize = True)
 
-fd_img.close()
+    fd_img.close()
 
 # End properly
 
